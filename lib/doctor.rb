8918ff3bd565de6278e15b6e9e59454c5be35828
Yuri Nguyen <yuringuyen312@gmail.com>
2021-02-26 19:39:29 -0500

    commit tree 81424806c65775083813dda606249711ef53769f

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,24 @@
+class Doctor
+    @@all = []
+    attr_accessor :name
+    def initialize(doctor_name)
+        @name = doctor_name
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select {|appointment| self == appointment.doctor}
+    end
+
+    def patients
+        appointments.map {|appointment| appointment.patient}
+    end
+end
